289bd3c821252565e8aa3be3bbe8d4361b4560ad
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2026-02-23 02:10:36 +0600

    Create database table

---
+++ Library.Infrastructure/Data/LibraryBookDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Library.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.Data
+{
+    public class LibraryBookDbContext : DbContext
+    {
+        public LibraryBookDbContext(DbContextOptions<LibraryBookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .ToTable("Book");
+
+            modelBuilder.Entity<Book>()
+                .HasIndex(b => b.ISBN)
+                .IsUnique();
+
+
+        }
+    }
+}
---
+++ Library.Infrastructure/Migrations/20260222180526_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    PublicationYear = table.Column<int>(type: "int", nullable: false),
+                    Pages = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    AddedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_ISBN",
+                table: "Book",
+                column: "ISBN",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
